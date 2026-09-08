bc3275561f5e153145d967e41f4f731be6f4889e
josh-grant-01 <Dragoon.Cahill@gmail.com>
2026-09-07 22:33:09 -0600

    Changed NPrograms arrary for testing purposes at home.

--- ARCH_INTD_INSTALL.ps1
+++ ARCH_INTD_INSTALL.ps1
@@ -25,25 +25,24 @@ if (-not $IsAdmin) {
 # --- End self-elevation block ---
 
 $global:Nprograms = @(
-    @{Name = "Ninite"; Loc = "\\ucsarch\apps`$\NINITE\Labs042026\Ninite 7Zip ASPNET Core Runtime 10 Installer.exe"; Para = "/silent C:\Logs\Ninite\Install.txt"; RequiresRestart = $false},
-    @{Name = "Nvidia App"; Loc = "\\ucsarch\apps`$\Drivers\NVIDIA_app_v11.0.6.383.exe"; Para = "/s"; RequiresRestart = $true},
-    @{Name = "Nvidia Driver"; Loc = "\\ucsarch\apps`$\Drivers\Nvidia_Stable_570_Channel\573.96-quadro-rtx-desktop-notebook-win10-win11-64bit-international-dch-whql.exe"; Para = "/s"; RequiresRestart = $true}
-)
-$global:Bprograms = @(
+    @{Name = "Ninite"; Loc = "\\JFLIPLT\Software\Ninite\Ninite 7Zip Audacity Chrome Discord Firefox Installer.exe"; Para = "/silent C:\Logs\Ninite\Install.txt"; RequiresRestart = $false},
+    @{Name = "Nvidia App"; Loc = "\\JFLIPLT\Software\Nvidia\App\NVIDIA_app_v11.0.8.299.exe"; Para = "/s"; RequiresRestart = $true},
+    # @{Name = "Nvidia Driver"; Loc = "\\ucsarch\apps`$\Drivers\Nvidia_Stable_570_Channel\573.96-quadro-rtx-desktop-notebook-win10-win11-64bit-international-dch-whql.exe"; Para = "/s"; RequiresRestart = $true}
+
     #Adobe CC 2026 Full Package
-    @{Name = "Adobe"; Loc = "\\artcomm\oit`$\Installers\Adobe\cc26\20260205-CC2026-SDL\Install2.cmd"; Para = "/c"; RequiresRestart = $false},
+    # @{Name = "Adobe"; Loc = "\\artcomm\oit`$\Installers\Adobe\cc26\20260205-CC2026-SDL\Install2.cmd"; Para = "/c"; RequiresRestart = $false},
     
     #Autodesk 2027 Full Package
-    @{Name = "Autodesk"; Loc = "\\ucsarch\apps$\Autodesk\2027\"; Para = ""; RequiresRestart = $false},
+    # @{Name = "Autodesk"; Loc = "\\ucsarch\apps$\Autodesk\2027\"; Para = ""; RequiresRestart = $false},
     
     #SketchUp Full 2026
-    @{Name = "SketchUp 2026"; $Loc = "\\ucsrch\apps`$\Sketchup\*SketchUp*"; Para = "/silent,/FEATURES=fr,de,es,it,ja,scan_essentials,revit_importer"; RequiresRestart = $false}
+    @{Name = "SketchUp 2026"; $Loc = "\\JFLIPLT\Software\SketchUp\SketchUp 2026\SketchUp-2026-2-243-76.exe"; Para = "/silent,/FEATURES=fr,de,es,it,ja,scan_essentials,revit_importer"; RequiresRestart = $false}
     
     #Lumion Student 2026
-    @{Name = "Lumion Student 2026"; Loc = "\\ucsarch\apps`$\Lumion\Lumion_2025_0_2_Student_Download.exe"; Para = "--silent --silentautoexit"; RequiresRestart = $false},
+    # @{Name = "Lumion Student 2026"; Loc = "\\ucsarch\apps`$\Lumion\Lumion_2025_0_2_Student_Download.exe"; Para = "--silent --silentautoexit"; RequiresRestart = $false},
     
     #Lumion Plugin for Revit 2027
-    @{Name = "Lumion Plugin for Revit 2027"; Loc = "\\ucsarch\apps`$\Lumion\Revit_LiveSync_Plugin_Installation.bat"; Para = ""; RequiresRestart = $false}
+    @{Name = "Lumion Plugin for Revit 2027"; Loc = "\\JFLIPLT\Software\Lumion\LumionPluginSIlentInstall.bat"; Para = ""; RequiresRestart = $false}
 )
 #$global:Sprograms = ($AppName = "Security Cert for Lumion" $InstallerPath = "\\ucsarch\apps$\" $InstallerArgs = "")
 $global:Pprograms = (Name = "Prusa Software" Loc = "\\ucsarch\apps$\Prusa\" Para = ""; RequiresRestart = $false)
